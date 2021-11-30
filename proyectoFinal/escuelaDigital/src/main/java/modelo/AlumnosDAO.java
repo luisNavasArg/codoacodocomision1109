93d4318fcbe9ba3bed2a068344b79f6a86caa543
luisNavasArg <lpipnavas@gmail.com>
2021-11-30 12:02:31 -0300

    clase AlumnosDAO creamos dos métodos faltan 3

--- proyectoFinal/escuelaDigital/src/main/java/modelo/AlumnosDAO.java
+++ proyectoFinal/escuelaDigital/src/main/java/modelo/AlumnosDAO.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author Usuario
- */
+import config.Conexion;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class AlumnosDAO {
+    Connection conexion;
+    public AlumnosDAO(){
+        Conexion con = new Conexion();
+        conexion = con.getConection();
+    }
+    public List<Alumnos> listarAlumnos(){
+        PreparedStatement ps;
+        ResultSet rs;
+        List<Alumnos> lista = new ArrayList<>();
+        /*List clase abstracta*/
+        try{
+            ps = conexion.prepareStatement("SELECT * FROM participantes");
+            rs = ps.executeQuery();
+            
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String nombres = rs.getString("nombres");
+                String apellidos = rs.getString("apellidos");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono"); 
+                Alumnos alumnos = new Alumnos(id,nombres,apellidos,
+                        email, telefono);
+                lista.add(alumnos);
+            }
+            
+            return lista;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+    
+    public Alumnos mostrarAlumno(int _id){
+        PreparedStatement ps;
+        ResultSet rs;
+        Alumnos alumno = null;
+        
+        try{
+            ps = conexion.prepareStatement("SELECT id, nombres,apellidos,"
+                    + "email,telefono FROM participantes "
+                    + "Where id = ?");
+            ps.setInt(1, _id);
+            rs = ps.executeQuery();
+            
+             while(rs.next()){
+                int id = rs.getInt("id");
+                String nombres = rs.getString("nombres");
+                String apellidos = rs.getString("apellidos");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+                alumno = new Alumnos(id,nombres,apellidos,
+                        email, telefono);
+             }
+             return alumno;
+            
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+    /*Faltan 3 métodos*/
     
 }
--- proyectoFinal/escuelaDigital/src/main/webapp/Vistas/modificar.jsp
+++ proyectoFinal/escuelaDigital/src/main/webapp/Vistas/modificar.jsp
@@ -14,10 +14,16 @@
     </head>
     <body>
         <h1 class="text-center">Modificar Registro</h1>
+        <!-- agregar el id oculto -->
         <div class="container">
             <div class="row d-flex justify-content-center">
                 <form class="p-5 w-50" action="AlumnosController?accion=insert"
                       method="POST">
+                    <div class="mb-3">
+                        <label for="id" class="form-label"></label>
+                        <input type="hidden" class="form-control" id="id"
+                               name="id" value=""/>
+                    </div>
                     <div class="mb-3">
                         <label for="nombre" class="form-label">Nombre</label>
                         <input type="text" class="form-control" id="nombre"
