edbf1ed8e87c438d338bfd95dacddc226aa912b1
luisNavasArg <lpipnavas@gmail.com>
2021-12-02 12:07:53 -0300

    Comenzamos con el controller

--- proyectoFinal/escuelaDigital/src/main/java/modelo/AlumnosDAO.java
+++ proyectoFinal/escuelaDigital/src/main/java/modelo/AlumnosDAO.java
@@ -68,6 +68,50 @@ public class AlumnosDAO {
             return null;
         }
     }
-    /*Faltan 3 métodos*/
+    public boolean insertarAlumno(Alumnos alumno){
+        PreparedStatement ps;
+        try{
+          ps = conexion.prepareStatement(
+  "INSERT INTO participantes(nombres,apellidos,email, telefono)VALUES(?,?,?,?)");
+          ps.setString(1,alumno.getNombres());
+          ps.setString(2,alumno.getApellidos());
+          ps.setString(3,alumno.getEmail());
+          ps.setString(4,alumno.getTelefono());
+          ps.execute();
+          return true;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
     
+    public boolean actualizarAlumno(Alumnos alumno){
+        PreparedStatement ps;
+         try{
+          ps = conexion.prepareStatement(
+  "UPDATE participantes SET nombres=?, apellidos=?, email=?,telefono=? WHERE id=?");
+          ps.setString(1,alumno.getNombres());
+          ps.setString(2,alumno.getApellidos());
+          ps.setString(3,alumno.getEmail());
+          ps.setString(4,alumno.getTelefono());
+          ps.setInt(5, alumno.getId());
+          ps.execute();
+          return true;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+    public boolean eliminarAlumno(int _id){
+        PreparedStatement ps;
+        try{
+          ps = conexion.prepareStatement("DELETE FROM participantes WHERE id=?");
+          ps.setInt(1, _id);
+          ps.execute();
+          return true;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
 }
---
+++ proyectoFinal/escuelaDigital/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 2 dic. 2021, 10:47:39
+    Author     : Usuario
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Bienvenid@s a Escuela Digital!</h1>
+    </body>
+</html>
