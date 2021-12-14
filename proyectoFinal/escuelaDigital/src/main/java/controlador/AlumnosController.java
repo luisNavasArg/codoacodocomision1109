5f554ae4c47fdbd770dd024c39f3787bee0a15b8
luisNavasArg <lpipnavas@gmail.com>
2021-12-14 12:12:02 -0300

    método actualizar

--- proyectoFinal/escuelaDigital/src/main/java/controlador/AlumnosController.java
+++ proyectoFinal/escuelaDigital/src/main/java/controlador/AlumnosController.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import modelo.Alumnos;
 import modelo.AlumnosDAO;
 
 @WebServlet(name = "AlumnosController", urlPatterns = {"/AlumnosController"})
@@ -24,6 +26,17 @@ public class AlumnosController extends HttpServlet {
                 dispatcher = request.getRequestDispatcher("Vistas/alumnos.jsp");
             }else if(accion.equals("modificar")){
                 dispatcher = request.getRequestDispatcher("Vistas/modificar.jsp");
+            }else if(accion.equals("insert")){
+                String nombre = request.getParameter("nombre");
+                String apellido = request.getParameter("apellido");
+                String email = request.getParameter("email");
+                String telefono = request.getParameter("telefono");
+
+                Alumnos alumno = new Alumnos(0,nombre,apellido,email,telefono); 
+                alum.actualizarAlumno(alumno);
+                dispatcher = request.getRequestDispatcher("Vistas/alumnos.jsp");
+            }else if(accion.equals("eliminar")){
+                
             }
             dispatcher.forward(request,response);
     }
